c3964997af52657e942590857545626a3dcfe6c7
softish <lazar.h.klincov@live.se>
2017-11-25 23:58:37 +0100

    Enabled production mode endpoints.

--- src/app/authentication.service.ts
+++ src/app/authentication.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import { Injectable, isDevMode } from '@angular/core';
 import { Http, Headers, RequestOptions, Response } from '@angular/http';
 import 'rxjs/add/operator/map';
 
@@ -11,14 +11,21 @@ export class AuthenticationService {
         username: '',
         password: ''
     };
+    
+    private authenticateEndpoint: string;
 
     constructor(private http: Http) {
+        if(isDevMode()) {
+            this.authenticateEndpoint = 'http://localhost:8080/user/authenticate';
+        } else {
+            this.authenticateEndpoint = 'http://zapto.ketchup.org:5757/user/authenticate';
+        }
     }
 
     authenticate(user: User) {
         const headers = new Headers({ 'Content-Type': 'application/json' });
         const options = new RequestOptions({ headers: headers });
-        return this.http.post('http://localhost:8080/user/authenticate', user)
+        return this.http.post(this.authenticateEndpoint, user)
         .map((response: Response) => response.json());
     }
 }
--- src/app/session.service.ts
+++ src/app/session.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import { Injectable, isDevMode } from '@angular/core';
 import { Http, Headers, RequestOptions, Response } from '@angular/http';
 import 'rxjs/add/operator/map';
 
@@ -8,31 +8,48 @@ import { SessionRange } from './session-range';
 
 @Injectable()
 export class SessionService {
+
+    private getLatestEndpoint: string;
+    private getRangeEndpoint: string;
+    private getDetailedRangeEndpoint: string;
+    private getDetailedForDateEndpoint: string;
+
     constructor(private http: Http) {
+        if(isDevMode()) {
+            this.getLatestEndpoint = 'http://localhost:8080/session/getLatest';
+            this.getRangeEndpoint = 'http://localhost:8080/session/getRange';
+            this.getDetailedRangeEndpoint = 'http://localhost:8080/session/getDetailedRange';
+            this.getDetailedForDateEndpoint = 'http://localhost:8080/session/getDetailedForDate';
+        } else {
+            this.getLatestEndpoint = 'http://zapto.ketchup.org:5757/session/getLatest';
+            this.getRangeEndpoint = 'http://zapto.ketchup.org:5757/getRange';
+            this.getDetailedRangeEndpoint = 'http://zapto.ketchup.org:5757/session/getDetailedRange';
+            this.getDetailedForDateEndpoint = 'http://zapto.ketchup.org:5757/session/getDetailedForDate';
+        }
     }
 
     getLatest(authenticatedUser: AuthenticatedUser) {
         const headers = new Headers({ 'Content-Type': 'application/json' });
         const options = new RequestOptions({ headers: headers });
-        return this.http.post('http://localhost:8080/session/getLatest', authenticatedUser).map((response: Response) => response.json());
+        return this.http.post(this.getLatestEndpoint, authenticatedUser).map((response: Response) => response.json());
     }
 
     getDurationInRange(sessionRange: SessionRange) {
         const headers = new Headers({ 'Content-Type': 'application/json' });
         const options = new RequestOptions({ headers: headers });
-        return this.http.post('http://localhost:8080/session/getRange', sessionRange).map((response: Response) => response.json());
+        return this.http.post(this.getRangeEndpoint, sessionRange).map((response: Response) => response.json());
     }
 
     getDetailedDurationInRange(sessionRange: SessionRange) {
         const headers = new Headers({ 'Content-Type': 'application/json' });
         const options = new RequestOptions({ headers: headers });
-        return this.http.post('http://localhost:8080/session/getDetailedRange', sessionRange).map((response: Response) => response.json());
+        return this.http.post(this.getDetailedRangeEndpoint, sessionRange).map((response: Response) => response.json());
     }
 
     getBreakdownForDate(sessionRange: SessionRange) {
         const headers = new Headers({ 'Content-Type': 'application/json' });
         const options = new RequestOptions({ headers: headers });
-        return this.http.post('http://localhost:8080/session/getDetailedForDate', sessionRange)
+        return this.http.post(this.getDetailedForDateEndpoint, sessionRange)
             .map((response: Response) => response.json());
     }
 }
